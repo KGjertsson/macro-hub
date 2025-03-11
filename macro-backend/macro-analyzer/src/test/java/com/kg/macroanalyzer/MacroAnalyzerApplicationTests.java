7813828b829fb66139dd1c2f99b751541a2b7c90
KGjertsson <gjertsson.konrad@gmail.com>
2025-03-11 09:00:42 +0100

    feat: add test containers for integration tests

--- macro-backend/macro-analyzer/src/test/java/com/kg/macroanalyzer/MacroAnalyzerApplicationTests.java
+++ macro-backend/macro-analyzer/src/test/java/com/kg/macroanalyzer/MacroAnalyzerApplicationTests.java
@@ -1,15 +1,29 @@
 package com.kg.macroanalyzer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 
 @SpringBootTest
-@ActiveProfiles("dev")
+@Testcontainers
+@ActiveProfiles("test")
 class MacroAnalyzerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void contextLoads() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            System.out.println("Connected to: " + connection.getMetaData().getURL());
+        }
+    }
 
 }
