5befdbaf540132b39a9a396076872be30d01c22e
KGjertsson <gjertsson.konrad@gmail.com>
2025-12-30 23:31:42 +0100

    refactor: refactor cleanup

--- macro-backend/macro-analyzer/src/main/java/com/kg/macroanalyzer/application/services/scrape/ScrapeFlowHandle.java
+++
@@ -1,7 +0,0 @@
-package com.kg.macroanalyzer.application.services.scrape;
-
-import com.kg.macroanalyzer.adaptors.database.postgres.models.ScrapeQueueItem;
-
-public interface ScrapeFlowHandle {
-    ScrapeResult run(ScrapeQueueItem item);
-}
